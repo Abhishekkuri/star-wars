df77bb90d0d267b12edabdc9bafb82ae3016ef97
Abhishek Kuri <36217364+Abhishekkuri@users.noreply.github.com>
2019-10-29 11:56:57 +0100

    Updated About Page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,59 +3,85 @@ import React from "react";
 const About = () => {
   return (
     <div>
+      <h1 id="welcometostarwarsapplication">
+        Welcome to Starwars Application!
+      </h1>
+
       <p>
-        Extremity sweetness difficult behaviour he of. On disposal of as
-        landlord horrible. Afraid at highly months do things on at. Situation
-        recommend objection do intention so questions. As greatly removed
-        calling pleased improve an. Last ask him cold feel met spot shy want.
-        Children me laughing we prospect answered followed. At it went is song
-        that held help face. Perhaps far exposed age effects. Now distrusts you
-        her delivered applauded affection out sincerity. As tolerably recommend
-        shameless unfeeling he objection consisted. She although cheerful
-        perceive screened throwing met not eat distance. Viewing hastily or
-        written dearest elderly up weather it as. So direction so sweetness or
-        extremity at daughters. Provided put unpacked now but bringing. Of
-        friendship on inhabiting diminution discovered as. Did friendly eat
-        breeding building few nor. Object he barton no effect played valley
-        afford. Period so to oppose we little seeing or branch. Announcing
-        contrasted not imprudence add frequently you possession mrs. Period saw
-        his houses square and misery. Hour had held lain give yet. Picture
-        removal detract earnest is by. Esteems met joy attempt way clothes yet
-        demesne tedious. Replying an marianne do it an entrance advanced. Two
-        dare say play when hold. Required bringing me material stanhill jointure
-        is as he. Mutual indeed yet her living result matter him bed whence. An
-        so vulgar to on points wanted. Not rapturous resolving continued
-        household northward gay. He it otherwise supported instantly. Unfeeling
-        agreeable suffering it on smallness newspaper be. So come must time no
-        as. Do on unpleasing possession as of unreserved. Yet joy exquisite put
-        sometimes enjoyment perpetual now. Behind lovers eat having length
-        horses vanity say had its. Did shy say mention enabled through elderly
-        improve. As at so believe account evening behaved hearted is. House is
-        tiled we aware. It ye greatest removing concerns an overcame appetite.
-        Manner result square father boy behind its his. Their above spoke match
-        ye mr right oh as first. Be my depending to believing perfectly
-        concealed household. Point could to built no hours smile sense. Are
-        sentiments apartments decisively the especially alteration. Thrown shy
-        denote ten ladies though ask saw. Or by to he going think order event
-        music. Incommode so intention defective at convinced. Led income months
-        itself and houses you. After nor you leave might share court balls. He
-        difficult contented we determine ourselves me am earnestly. Hour no find
-        it park. Eat welcomed any husbands moderate. Led was misery played
-        waited almost cousin living. Of intention contained is by middleton am.
-        Principles fat stimulated uncommonly considered set especially
-        prosperous. Sons at park mr meet as fact like. At distant inhabit
-        amongst by. Appetite welcomed interest the goodness boy not. Estimable
-        education for disposing pronounce her. John size good gay plan sent old
-        roof own. Inquietude saw understood his friendship frequently yet.
-        Nature his marked ham wished. Both rest of know draw fond post as. It
-        agreement defective to excellent. Feebly do engage of narrow. Extensive
-        repulsive belonging depending if promotion be zealously as. Preference
-        inquietude ask now are dispatched led appearance. Small meant in so
-        doubt hopes. Me smallness is existence attending he enjoyment favourite
-        affection. Delivered is to ye belonging enjoyment preferred. Astonished
-        and acceptance men two discretion. Law education recommend did objection
-        how old.
+        Hi! <strong>Starwars</strong> fans. This is a Simple Starwars Web
+        Application written in ReactJS.
       </p>
+
+      <h1 id="functionalities">Functionalities</h1>
+
+      <p>Starwars application has following functionalities in it :</p>
+
+      <ul>
+        <li>
+          A <strong>Logo</strong> on top which redirects to Starwars website.
+        </li>
+
+        <li>
+          An Independent <strong>Header</strong> component to show the Navbar
+          and links to go to Search or About pages.
+        </li>
+
+        <li>
+          Routing is done with <strong>React-Router</strong>.
+        </li>
+
+        <li>
+          It has an Independent <strong>Search</strong> component to search for
+          characters.
+        </li>
+
+        <li>
+          A <strong>Button with Spinner</strong> functionality in it to show the
+          processing status.
+        </li>
+
+        <li>
+          On <strong>successful search</strong>, it shows up to 5 characters and
+          the total Count of Characters.
+        </li>
+
+        <li>
+          An <strong>Error</strong> component is included to handle the error
+          massaged in case of any error being thrown from Backend.
+        </li>
+
+        <li>
+          Count, Characters' details are in separate component along with Film
+          component which is embedded inside the characters' detail component.
+        </li>
+
+        <li>
+          Data is fetched with <strong>Axios</strong> library.
+        </li>
+
+        <li>
+          <strong>Lodash</strong> library is used to convert objects into an
+          array.
+        </li>
+
+        <li>
+          The UI is made with responsiveness. <strong>Flexbox</strong> is used
+          for the purpose.
+        </li>
+
+        <li>About Page contains the same Information.</li>
+
+        <li>
+          At the end of the page <strong>Footer</strong> is included as an
+          independent component to have all the social information about me(Plz
+          feel free to contact me any time).
+        </li>
+
+        <li>
+          UI is given a simple theme of Starwars which is{" "}
+          <strong>Black and Orange</strong>.
+        </li>
+      </ul>
     </div>
   );
 };
